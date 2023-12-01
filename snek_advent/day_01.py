8b8f8a9c218244e50b01b4588b93bcf395459300
raf <raf@akira.localdomain>
2023-12-01 20:48:20 +0100

    optimisations day01

--- snek_advent/day_01.py
+++ snek_advent/day_01.py
@@ -1,73 +1,78 @@
 import re
+from cProfile import Profile
 from functools import reduce
+from pstats import Stats, SortKey
+from timeit import timeit
 
 written_out = {
-    "1": ["one", "1"],
-    "2": ["two", "2"],
-    "3": ["three", "3"],
-    "4": ["four", "4"],
-    "5": ["five", "5"],
-    "6": ["six", "6"],
-    "7": ["seven", "7"],
-    "8": ["eight", "8"],
-    "9": ["nine", "9"],
-}
-
-written_out_reversed = {
-    "1": ["one"[::-1], "1"],
-    "2": ["two"[::-1], "2"],
-    "3": ["three"[::-1], "3"],
-    "4": ["four"[::-1], "4"],
-    "5": ["five"[::-1], "5"],
-    "6": ["six"[::-1], "6"],
-    "7": ["seven"[::-1], "7"],
-    "8": ["eight"[::-1], "8"],
-    "9": ["nine"[::-1], "9"],
+    1: ["one", "1", "one"[::-1]],
+    2: ["two", "2", "two"[::-1]],
+    3: ["three", "3", "three"[::-1]],
+    4: ["four", "4", "four"[::-1]],
+    5: ["five", "5", "five"[::-1]],
+    6: ["six", "6", "six"[::-1]],
+    7: ["seven", "7", "seven"[::-1]],
+    8: ["eight", "8", "eight"[::-1]],
+    9: ["nine", "9", "nine"[::-1]],
 }
 
 compiled_regex = re.compile(r"\d")
 
 
 def part01(lines):
-    print(
-        reduce(
-            (lambda p, n: p + (int("".join([n[0], n[-1]])))),
-            list(map((lambda x: compiled_regex.findall(x)), lines)),
-            0,
-        )
+    return reduce(
+        (lambda p, n: p + int(f"{n[0]}{n[-1]}")),
+        map((lambda x: compiled_regex.findall(x)), lines),
+        0,
     )
-
-
-def get_index(line, tgt):
-    return line.index(tgt) if tgt in line else -1
 
 
 def part02_parse_line(line, reversed_line):
     first_number_found, last_number_found, first_item_index, last_item_index = (
-        "-1",
-        "-1",
+        -1,
+        -1,
         10_000,
         10_000,
     )
+
     for wo in written_out:
-        for val in written_out[wo]:
-            ff_index = get_index(line, val)
+        for val in written_out[wo][0:2]:
+            ff_index = line.index(val) if val in line else -1
             if ff_index != -1 and ff_index < first_item_index:
                 first_item_index = ff_index
                 first_number_found = wo
 
-        for val_rev in written_out_reversed[wo]:
-            fl_index = get_index(reversed_line, val_rev)
+        for val in written_out[wo][1:]:
+            fl_index = reversed_line.index(val) if val in reversed_line else -1
             if fl_index != -1 and fl_index < last_item_index:
                 last_item_index = fl_index
                 last_number_found = wo
 
-    return int("".join([first_number_found, last_number_found]))
+    return last_number_found + (10 * first_number_found)
 
 
 def part02(lines):
-    print(
-        reduce(
-            lambda x, y: x + y, (map(lambda l: part02_parse_line(l, l[::-1]), lines))
-        )
+    return reduce(
+        lambda x, y: x + y, map(lambda l: part02_parse_line(l, l[::-1]), lines)
     )
+
+
+def do(iterations, lines, do_profile=False):
+    total_time = timeit(lambda: part01(lines), number=iterations, globals=globals())
+    print(
+        f"Average time is {total_time / iterations:.10f} seconds ({iterations} iterations)"
+    )
+
+    total_time = timeit(lambda: part02(lines), number=iterations, globals=globals())
+    print(
+        f"Average time is {total_time / iterations:.10f} seconds ({iterations} iterations)"
+    )
+
+    if do_profile:
+        with Profile() as profile:
+            print(f"{part01(lines) = }")
+            (Stats(profile).strip_dirs().sort_stats(SortKey.CALLS).print_stats())
+
+        with Profile() as profile:
+            print(f"{part02(lines) = }")
+            (Stats(profile).strip_dirs().sort_stats(SortKey.CALLS).print_stats())
--- starter.py
+++ starter.py
@@ -1,12 +1,7 @@
-import snek_advent.day_01 as day01
+from snek_advent.day_01 import do
 from snek_advent.input import as_lines
-from datetime import datetime
 
+iterations = 1_000
 lines = as_lines()
-start=datetime.now()
-day01.part01(lines) #54597
-print("part1:\t", datetime.now() - start)
 
-start=datetime.now()
-day01.part02(lines) #54504
-print("part2:\t", datetime.now() - start)
+do(iterations, lines)

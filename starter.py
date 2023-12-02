ad7f53ce5d5fb45c17307d3cf02e8ef58be50126
raf <rceuls@gmail.com>
2023-12-02 19:34:58 +0100

    blep

--- starter.py
+++ starter.py
@@ -1,4 +1,5 @@
 from cProfile import Profile
+from datetime import datetime
 from pstats import Stats
 
 from snek_advent.day_01 import do as day01_do
@@ -6,19 +7,20 @@ from snek_advent.day_02 import do as day02_do
 from snek_advent.day_03 import do as day03_do
 
 iterations = 0
-run_everything = True
+run_everything = False
+day = datetime.now().day
 do_profile = False
 
 with Profile() as profile:
-    if run_everything:
+    if run_everything or day == 1:
         with open("./resx/day01.txt", "r") as f:
             print("🧝‍♀️🧝‍♀️🧝‍♀️ day 01 🧝‍♀️🧝‍♀️🧝‍♀️")
             day01_do(iterations, f.readlines(), do_profile)
-    if run_everything:
+    if run_everything or day == 2:
         print("🎅🎅🎅 day 02 🎅🎅🎅")
         with open("./resx/day02.txt", "r") as f:
             day02_do(iterations, f.readlines(), do_profile)
-    if run_everything:
+    if run_everything or day == 3:
         print("☃️☃️☃️ day 03 ☃️☃️☃️")
         with open("./resx/day03.txt", "r") as f:
             day03_do(iterations, f.readlines(), do_profile)

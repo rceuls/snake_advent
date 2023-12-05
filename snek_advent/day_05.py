9eb78ad6b4ae0885aca08688ff1ea7ee5fac2a42
Raf Ceuls <raf.ceuls@persgroep.net>
2023-12-05 15:43:53 +0100

    fifth day

--- snek_advent/day_05.py
+++ snek_advent/day_05.py
@@ -1,3 +1,5 @@
+from functools import partial
+from multiprocessing import Pool
 import re
 from cProfile import Profile
 from pstats import Stats, SortKey
@@ -12,6 +14,7 @@ def parse_input(full_text):
     splitted_by_converter = full_text.split("\n\n")
     for seed in digit_regex.finditer(splitted_by_converter[0]):
         mapper["seeds"].append(int(seed.group(0)))
+
     for conv_ix in range(1, len(splitted_by_converter)):
         conv = splitted_by_converter[conv_ix]
         splitted_converter = conv.split("\n")
@@ -27,36 +30,42 @@ def parse_input(full_text):
             }
 
         mapper["converters"][source] = converters
-    print(mapper)
     return mapper
 
 
 def parse_target(mapper, medium, origin):
     if medium in mapper:
+        new_medium = mapper[medium]["target"]
         for key in mapper[medium]["ranges"].keys():
             if key[0] <= origin and key[1] >= origin:
                 dest = mapper[medium]["ranges"][key]["dest"]
                 offset = origin - key[0]
-                print(
-                    medium,
-                    "->",
-                    new_medium,
-                    origin,
-                    key,
-                    dest,
-                    "+",
-                    offset,
-                    "=",
-                    dest + offset,
-                )
-                new_medium = mapper[medium]["target"]
                 return parse_target(mapper, new_medium, dest + offset)
         return parse_target(mapper, new_medium, origin)
     return origin
 
 
 def part02(lines):
-    pass
+    mapper = parse_input(lines)
+    old_seeds = mapper["seeds"]
+    seeds = []
+    for x in range(0, len(old_seeds) - 1):
+        if x % 2 == 0:
+            baseline = old_seeds[x]
+            amount = old_seeds[x + 1]
+            for ss in range(baseline, amount):
+                seeds.append(ss + baseline)
+
+    print("ello")
+    lowest = 2**31
+    func = partial(parse_target, mapper["converters"], "seed")
+
+    with Pool() as p:
+        p.map(func, seeds)
+        p.close()
+        p.join()
+
+    return lowest
 
 
 def part01(lines):
@@ -64,7 +73,6 @@ def part01(lines):
     lowest = 2**31
     for seed in mapper["seeds"]:
         tgt = parse_target(mapper["converters"], "seed", seed)
-        exit(0)
         if tgt < lowest:
             lowest = tgt
 
@@ -85,7 +93,7 @@ def do(iterations, lines, do_profile=False):
         )
 
     with Profile() as profile:
-        print(f"{part01(lines) = } (should be 23941)")
+        print(f"{part01(lines) = } (should be 26273516)")
         if do_profile:
             (Stats(profile).strip_dirs().sort_stats(SortKey.CALLS).print_stats())
 

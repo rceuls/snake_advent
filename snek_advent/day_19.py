27480bfc763873b5f4d9f505ae543368da7d6bf4
Raf Ceuls <rceuls@users.noreply.github.com>
2023-12-19 22:46:44 +0100

    day 19 blergh

--- snek_advent/day_19.py
+++ snek_advent/day_19.py
@@ -1,4 +1,6 @@
+from collections import namedtuple
 from functools import partial
+from math import prod
 
 from snek_advent import validate
 
@@ -9,7 +11,10 @@ def smaller(wf_item, label, outcome, value):
 
 def bigger(wf_item, label, outcome, value):
     return outcome if wf_item < value[label] else None
-g
+
+
+Rule = namedtuple("Rule", "category valid_range target_wf")
+
 
 class Workflow:
     def __init__(self, raw):
@@ -17,14 +22,32 @@ class Workflow:
         self.label = raw.split("{")[0]
         raw_predicates = raw[len(self.label) : -1].split(",")
         predicates = []
+        self.ranges = []
         for p in raw_predicates[0:-1]:
             (predicate, outcome) = p.replace("{", "}").replace("}", "").split(":")
             op = "<" if "<" in predicate else ">"
             (target, value) = predicate.split(op)
             if op == "<":
                 predicates.append(partial(smaller, int(value), target, outcome))
+                self.ranges.append(
+                    Rule(
+                        valid_range=range(1, int(value)),
+                        target_wf=outcome,
+                        category=target,
+                    )
+                )
             else:
                 predicates.append(partial(bigger, int(value), target, outcome))
+                self.ranges.append(
+                    Rule(
+                        valid_range=range(int(value) + 1, 4001),
+                        target_wf=outcome,
+                        category=target,
+                    )
+                )
+        # for l in "xmas":
+        #     if l not in self.ranges:
+        #         self.ranges[l] = range(0, 4001)
         self.predicates = predicates
         self.default = raw_predicates[-1]
 
@@ -85,10 +108,52 @@ def part01(lines: list[str]):
     for p in parts:
         total += handle(p, wfd, wfd["in"])
 
-    validate(total, 263678)
+    # validate(total, 263678)
+
+
+def update_range(d, key, value):
+    new = dict()
+    for k, v in d.items():
+        new[k] = value if k == key else v
+    return new
 
 
 def part02(lines: list[str]):
     (wfd, _) = get_lines_and_workflows(lines)
+    q = [("in", {c: range(1, 4001) for c in "xmas"})]
+    result = 0
+    while q:
+        workflow_id, ranges = q.pop()
+        if workflow_id == "A":
+            result += prod(len(r) for r in ranges.values())
+        elif workflow_id in wfd:
+            workflow = wfd[workflow_id]
+            for category, valid_range, target_workflow in workflow.ranges:
+                new_range = range(
+                    max(valid_range.start, ranges[category].start),
+                    min(valid_range.stop, ranges[category].stop),
+                )
+                if new_range:
+                    q.append(
+                        (
+                            target_workflow,
+                            update_range(ranges, category, new_range),
+                        )
+                    )
+                old_range_inv = (
+                    range(valid_range.stop, 4001)
+                    if valid_range.start <= 1
+                    else range(1, valid_range.start)
+                )
 
+                old_range = range(
+                    max(old_range_inv.start, ranges[category].start),
+                    min(old_range_inv.stop, ranges[category].stop),
+                )
+                if not old_range:
+                    break
+                ranges[category] = old_range
+            else:
+                q.append((workflow.default, ranges))
+    print(result)
     validate(0, 0)

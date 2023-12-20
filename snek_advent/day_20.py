6af369e29af091c2bff689a84ac29022dbc22725
Raf Ceuls <rceuls@users.noreply.github.com>
2023-12-20 21:11:00 +0100

    day 20

--- snek_advent/day_20.py
+++ snek_advent/day_20.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
-from math import prod
+from math import prod, lcm
 
 from snek_advent import validate
 
@@ -20,14 +20,14 @@ class Node:
         self.label = label[1:] if label != "broadcaster" else "broadcaster"
         self.children = targets.split(", ")
         self.current_state = 0
-        self.parents = []
+        self.memory = {}
 
     def set_family(self, nodes):
         missing_nodes = []
         for node in nodes.values():
             for tgt in node.children:
                 if tgt == self.label:
-                    self.parents.append(tgt)
+                    self.memory[node.label] = 0
         for child in self.children:
             if child not in nodes:
                 nodes[child] = Node("_" + child + " -> ")
@@ -41,32 +41,33 @@ class Node:
         return f"Node({self.label} - {self.node_type})"
 
 
-Action = namedtuple("Action", "node_label pulse")
+Action = namedtuple("Action", "target pulse sender")
 
 
 def pulse(nodes, pulses):
-    q = [Action("broadcaster", 0)]
+    q = [Action("broadcaster", 0, "")]
+    historic_pulses = []
     while q:
-        node_label, state = q.pop()
+        target, state, sender = q.pop(0)
+        historic_pulses.append(Action(target, state, sender))
         pulses[state] += 1
-        node = nodes[node_label]
+        node = nodes[target]
+
         if node.node_type == "FLIP_FLOP" and state == 0:
             node.current_state = 0 if node.current_state == 1 else 1
             for child in node.children:
-                q.append(Action(child, node.current_state))
+                q.append(Action(child, node.current_state, target))
         elif node.node_type == "CONJUNCTION":
-            to_send = 0
-            for x in node.parents:
-                if nodes[x].current_state == 0:
-                    to_send = 1
-                    break
+            node.memory[sender] = state
+            to_send = 0 if all(node.memory.values()) else 1
             for child in node.children:
-                q.append((child, to_send))
+                q.append((child, to_send, target))
         elif node.node_type == "START":
             for child in node.children:
-                q.append(Action(child, state))
+                q.append(Action(child, state, target))
         else:
             pass
+    return historic_pulses
 
 
 def format_state(nodes):
@@ -86,6 +87,7 @@ def part01(lines: list[str]):
     prev_pulses = pulses.copy()
     loops = 1000
     for _ in range(0, loops):
+        prev_pulses = pulses.copy()
         pulse(nodes, pulses)
         state = format_state(nodes_list)
         if state in full_states:
@@ -94,18 +96,37 @@ def part01(lines: list[str]):
         prev_pulses = pulses.copy()
 
     division_bell = int(loops / len(full_states))
-    print(
+    validate(
         prod(
             [
                 prev_pulses[0] * division_bell,
                 prev_pulses[1] * division_bell,
             ]
-        )
+        ),
+        883726240,
     )
-    # low : 659508123 657110090
-    # high: 65711009000
-    validate(0, 0)
 
 
 def part02(lines: list[str]):
-    validate(0, 0)
+    nodes_list = [Node(x) for x in lines]
+    nodes = {k.label: k for k in nodes_list}
+    pulses = {1: 0, 0: 0}
+    loops = 0
+    first_high_pulse = {}
+    for node in nodes_list:
+        nodes_list += node.set_family(nodes)
+        for ch in node.children:
+            if ch == "bb":
+                first_high_pulse[node.label] = 0
+
+    while not all(first_high_pulse.values()):
+        loops += 1
+        loop_pulses = pulse(nodes, pulses)
+        for p in loop_pulses:
+            if (
+                p.pulse == 1
+                and p.sender in first_high_pulse
+                and not first_high_pulse[p.sender]
+            ):
+                first_high_pulse[p.sender] = loops
+    validate(lcm(*first_high_pulse.values()), 211712400442661)
